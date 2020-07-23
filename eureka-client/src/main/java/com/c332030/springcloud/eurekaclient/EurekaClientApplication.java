d7ac53ed23c3fdb4aae340b87750a36fd02fabd0
c332030 <c332030@gmail.com>
2020-07-23 23:20:39 +0800

    init

---
+++ eureka-client/src/main/java/com/c332030/springcloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package com.c332030.springcloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceHiApplication.class, args );
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
---
+++ eureka-client/src/test/java/com/c332030/springcloud/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.c332030.springcloud.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
