41e0945eb715661204ea3df58e932f7fa0c31898
c332030 <c332030@gmail.com>
2020-07-24 08:35:05 +0800

    fix: 错误修复

--- eureka-client/src/main/java/com/c332030/springcloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/c332030/springcloud/eurekaclient/EurekaClientApplication.java
@@ -1,15 +1,21 @@
 package com.c332030.springcloud.eurekaclient;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class ServiceHiApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( ServiceHiApplication.class, args );
+        SpringApplication.run( EurekaClientApplication.class, args );
     }
 
     @Value("${server.port}")
