0db474f114498db1ef89381eafa60349fe57cc40
c332030 <c332030@gmail.com>
2020-09-13 14:21:55 +0800

    feat: 断路器监控

---
+++ 20-server-hi-for-hystrix-dashboard/src/test/java/com/c332030/springcloud/eurekaclient/C21ServerHiForHystrixDashboardTests.java
@@ -0,0 +1,14 @@
+package com.c332030.springcloud.eurekaclient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class C21ServerHiForHystrixDashboardTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
